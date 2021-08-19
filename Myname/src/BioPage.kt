2675eac872e96aa26bef9c0d3d85c922fba58a3d
Olufemi Ezekiel <ororo17@gmail.com>
2021-08-19 13:55:13 +0100

    BioPage
    A function that prints my name

---
+++ Myname/src/BioPage.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var lastname = "OLUFEMI"
+    var firstName = "EZEKIEL"
+    whatIsYourName(lastname, firstName)
+}
+
+fun whatIsYourName(lastName: String, firstName: String) {
+
+    print("My name is $lastName $firstName")
+
+}
